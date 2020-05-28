dbf5011d1cee65e53d19fce81c07b1b3848c3424
DiogoCostaPT <1208buckas>
2020-05-28 13:04:23 -0600

    fixes to the previous commit

--- src/process_json.cpp
+++ src/process_json.cpp
@@ -21,33 +21,54 @@ void jsonconfig_dim(const std::string& configjson_file)
     cmpt_on = false;
     while (std::getline(file, str)) 
     {
-
+        // check the comand
          if(str.find("compartments") != std::string::npos){
              cmpt_on = true;
              dim_on = false;
              chem_on = false;
-             count = 0;
+             count = 1;
              continue;
              }; 
          if(str.find("dimensions") != std::string::npos){
              cmpt_on = false;
              dim_on = true;
              chem_on = false;
-             count = 0;
+             count = 1;
              continue;
              }; 
          if(str.find("chemical_species") != std::string::npos){
              cmpt_on = false;
              dim_on = false;
              chem_on = true;
-             count = 0;
+             count = 1;
              continue;
              }; 
          
+         // compartments
          if (cmpt_on){
-             count++;
-             count_str = '"' + std::to_string(count) +'"';
-             if(str.find(count_str) != std::string::npos){cmt_num++;continue;};
+             count_str = std::to_string(count);
+             if(str.find(count_str) != std::string::npos){
+                 cmt_num++;
+                 count++;
+                 continue;};
+         };
+
+        // dimensions
+         if (dim_on){
+             count_str = std::to_string(count);
+             if(str.find("n_x") != std::string::npos){
+                 n_x = ++;
+                 n_y = ;
+                 continue;};
+         };
+
+        // chemical species
+         if (chem_on){
+             count_str = std::to_string(count);
+             if(str.find(count_str) != std::string::npos){
+                 chem_num++;
+                 count++;
+                 continue;};
          }
 
     }
