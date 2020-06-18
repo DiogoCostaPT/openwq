d10fa2bedf4c97ac6cdc8604958a3597b9a57bc4
DiogoCostaPT <1208buckas>
2020-06-17 19:53:21 -0600

    Unstructured grid with multiple objects: working

--- src/writeVTU.cpp
+++ src/writeVTU.cpp
@@ -56,46 +56,60 @@ void writeVTU(JSONfiles& JSONfiles,int icmp)
   // As an exercise you can modify the coordinates of the points to create
   // seven topologically distinct convex hexahedras.
 
-  int numberOfVertices = 8;
+ int i;
+  static double x[27][3]={{0,0,0}, {1,0,0}, {2,0,0}, {0,1,0}, {1,1,0}, {2,1,0},
+                          {0,0,1}, {1,0,1}, {2,0,1}, {0,1,1}, {1,1,1}, {2,1,1},
+                          {0,1,2}, {1,1,2}, {2,1,2}, {0,1,3}, {1,1,3}, {2,1,3},
+                          {0,1,4}, {1,1,4}, {2,1,4}, {0,1,5}, {1,1,5}, {2,1,5},
+                          {0,1,6}, {1,1,6}, {2,1,6}};
+  static vtkIdType pts[12][8]={{0, 1, 4, 3, 6, 7, 10, 9},
+                               {1, 2, 5, 4, 7, 8, 11, 10},
+                               {6, 10, 9, 12, 0, 0, 0, 0},
+                               {8, 11, 10, 14, 0, 0, 0, 0},
+                               {16, 17, 14, 13, 12, 15, 0, 0},
+                               {18, 15, 19, 16, 20, 17, 0, 0},
+                               {22, 23, 20, 19, 0, 0, 0, 0},
+                               {21, 22, 18, 0, 0, 0, 0, 0},
+                               {22, 19, 18, 0, 0, 0, 0, 0},
+                               {23, 26, 0, 0, 0, 0, 0, 0},
+                               {21, 24, 0, 0, 0, 0, 0, 0},
+                               {25, 0, 0, 0, 0, 0, 0, 0}};
 
-// Write file
+
+
+
+  vtkSmartPointer<vtkPoints> points =
+    vtkSmartPointer<vtkPoints>::New();
+  for (i=0; i<27; i++) points->InsertPoint(i,x[i]);
+
+  vtkSmartPointer<vtkUnstructuredGrid> ugrid =
+    vtkSmartPointer<vtkUnstructuredGrid>::New();
+  ugrid->Allocate(100);
+  ugrid->InsertNextCell(VTK_HEXAHEDRON, 8, pts[0]);
+  ugrid->InsertNextCell(VTK_HEXAHEDRON, 8, pts[1]);
+  ugrid->InsertNextCell(VTK_TETRA, 4, pts[2]);
+  ugrid->InsertNextCell(VTK_TETRA, 4, pts[3]);
+  ugrid->InsertNextCell(VTK_POLYGON, 6, pts[4]);
+  ugrid->InsertNextCell(VTK_TRIANGLE_STRIP, 6, pts[5]);
+  ugrid->InsertNextCell(VTK_QUAD, 4, pts[6]);
+  ugrid->InsertNextCell(VTK_TRIANGLE, 3, pts[7]);
+  ugrid->InsertNextCell(VTK_TRIANGLE, 3, pts[8]);
+  ugrid->InsertNextCell(VTK_LINE, 2, pts[9]);
+  ugrid->InsertNextCell(VTK_LINE, 2, pts[10]);
+  ugrid->InsertNextCell(VTK_VERTEX, 1, pts[11]);
+
+  ugrid->SetPoints(points);
+
+
+  //return EXIT_SUCCESS;
+
+      // Write file
     vtkSmartPointer<vtkXMLUnstructuredGridWriter> writer =
         vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();  
     writer->SetFileName(filename.c_str());
     
-
-for (int i = 0; i < 2; ++i)
-    {
-  // Create the points
-  vtkSmartPointer<vtkPoints> points =
-    vtkSmartPointer<vtkPoints>::New();
-  points->InsertNextPoint(0.0+i, 0.0+i, 0.0+i);
-  points->InsertNextPoint(1.0+i, 0.0+i, 0.0+i);
-  points->InsertNextPoint(1.0+i, 1.0+i, 0.0+i);
-  points->InsertNextPoint(0.0+i, 1.0+i, 0.0+i);
-  points->InsertNextPoint(0.0+i, 0.0+i, 1.0+i);
-  points->InsertNextPoint(1.0+i, 0.0+i, 1.0+i);
-  points->InsertNextPoint(1.0+i, 1.0+i, 1.0+i);
-  points->InsertNextPoint(0.0+i, 1.0+i, 1.0+i);
-  
-
-  // Create a hexahedron from the points
-  vtkSmartPointer<vtkHexahedron> hex =
-    vtkSmartPointer<vtkHexahedron>::New();
-  for (int i = 0; i < numberOfVertices; ++i)
-    {
-    hex->GetPointIds()->SetId(i, i);
-    }
-
-  // Add the points and hexahedron to an unstructured grid
-  vtkSmartPointer<vtkUnstructuredGrid> uGrid =
-    vtkSmartPointer<vtkUnstructuredGrid>::New();
-  uGrid->SetPoints(points);
-  uGrid->InsertNextCell(hex->GetCellType(), hex->GetPointIds());
-    
-    writer->SetInputData(uGrid);
+    writer->SetInputData(ugrid);
     writer->Write();
-    }
 
 // Display file without the need of Parview
 /*
--- src/writeVTU.h
+++ src/writeVTU.h
@@ -3,7 +3,7 @@
 #define WRITEVTUH_INCLUDED
 
 #include <vtkSmartPointer.h>
-#include <vtkCellArray.h>
+//#include <vtkCellArray.h>
 //#include <vtkXMLUnstructuredGridReader.h>
 //#include <vtkDataSetMapper.h>
 //#include <vtkActor.h>
@@ -11,12 +11,21 @@
 //#include <vtkRenderWindow.h>
 //#include <vtkRenderWindowInteractor.h>
 #include <vtkXMLUnstructuredGridWriter.h>
-#include <vtkUnstructuredGrid.h>
-#include <vtkPointData.h>
+//#include <vtkUnstructuredGrid.h>
+//#include <vtkPointData.h>
 //#include <vtkVertexGlyphFilter.h>
 
-#include <vtkHexagonalPrism.h>
-#include <vtkHexahedron.h>
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkCellType.h>
+#include <vtkDataSetMapper.h>
+#include <vtkNamedColors.h>
+#include <vtkPoints.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkUnstructuredGrid.h>
 
 #include "global.h"
 
