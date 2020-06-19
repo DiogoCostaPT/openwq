516c892bc42f61ef246b76be3d4b3834a467ca48
DiogoCostaPT <1208buckas>
2020-06-18 18:24:23 -0600

    writeVTK: save all concentration variables to VTK: works for any number of compartments and chemical species

--- src/utility.cpp
+++ src/utility.cpp
@@ -53,19 +53,14 @@ void initmemory(JSONfiles& JSONfiles,Prj_StateVar& Prj_StateVar)
                          (*Prj_StateVar.wflux)(i) = domain_field;
 
                         // chemass
-                        numspec_j = 0;
-                        for (int j=0;j<numlistspec;j++){ 
+                        numspec_j = JSONfiles.BGC["compartments"][std::to_string(i+1)]["chem_species"].size(); 
 
-                            // number of species in compartment j
-                            numspec_j = JSONfiles.BGC["compartments"][std::to_string(i+1)]["chem_species"].size(); 
-
-                            // creating arma::field of arma:c:cube of size numspec_j-> copies of domain_xyz for each species
-                            arma::field<arma::Cube<double>> domain_field(numspec_j);
-                            for (int s=0;s<numspec_j;s++){
-                                domain_field(s) = domain_xyz;
-                            }
-                            (*Prj_StateVar.chemass)(i) = domain_field;
+                        // creating arma::field of arma:c:cube of size numspec_j-> copies of domain_xyz for each species
+                        arma::field<arma::Cube<double>> domain_field_2(numspec_j);
+                        for (int s=0;s<numspec_j;s++){
+                            domain_field_2(s) = domain_xyz;
                         }
+                        (*Prj_StateVar.chemass)(i) = domain_field_2;
         }
 }
 
