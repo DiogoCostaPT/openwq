f21cd708fee4ded6c09918f23ab6f2d1f55a9c7e
DiogoCostaPT <1208buckas>
2020-06-23 22:34:37 -0600

    renaming of function and cpp+h file names: transp_solver to main_solver

---
+++ src/main_solver.h
@@ -0,0 +1,18 @@
+
+#ifndef TRANSPSOLVERH_INCLUDED
+#define TRANSPSOLVERH_INCLUDED
+
+#include <armadillo>
+#include "string.h"
+#include <algorithm>
+
+#include "global.h"
+#include "utility.h"
+#include "readSetFluxes.h"
+#include "main_solver.h"
+#include "writeVTU.h"
+#include "ChemTransf.h"
+
+void main_solver(JSONfiles& JSONfiles,Prj_StateVar& Prj_StateVar);
+
+#endif
