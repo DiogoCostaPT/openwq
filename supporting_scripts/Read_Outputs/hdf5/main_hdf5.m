46c5835c88e94db91e409e14419dd7b4393e6329
DiogoCostaPT <1208buckas>
2021-09-22 19:10:04 -0600

    convert main_hf5.m into a callable function (so that we don't have to change its main code)

--- supporting_scripts/Read_Outputs/hdf5/main_hdf5.m
+++ supporting_scripts/Read_Outputs/hdf5/main_hdf5.m
@@ -15,63 +15,76 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-% Path to HDF5 folder
-folderpath = '/media/dcosta/data/megasync/ec_main/models/crhm/code_crhm/build/Output_OpenWQ/HDF5/';
 
-plot_elemt_flag = true;
+% Read and plot HDF5 output data
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Read h5 data and save to timeseries collection
-output_tscollect = Read_h5_save_tscollection(folderpath);
-   
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Get names of timeseries in tscollection
-tsnames = Get_output_tsnames(output_tscollect);
+function [tsnames,output_tscollect] = main_hdf5(...
+    folderpath,...      % Provide fullpath to directory where the HDF5 files are located
+    plot_elemt_flag,... % Flag to specify if to plot variables
+    plotElm_info...    % If the flag above is 1, then provide details about the variables to plot
+    )
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Plot elements
-if plot_elemt_flag
+    %{ 
+        Examples of input arguments
     
-    % get element info
-    plotElm_info = {...
-        'SOIL_RECHR_NO3','all';...
-        'SOIL_RECHR_NH4',[1,1,1;...
-                          6,1,1]...
-    };
+        folderpath = '/media/dcosta/data/megasync/ec_main/models/crhm/code_crhm/build/Case_Studies/stc_openwq/Output_OpenWQ/HDF5/';
+
+        plot_elemt_flag = true;
     
-    % plot requested data defined in plotElm_info
-    plot_elements(...
-        tsnames,...
-        output_tscollect,...
-        plotElm_info)
+        plotElm_info = {...
+            'SOIL_RECHR_LABILEP','all';...
+            'SOIL_RECHR_REFRACTORYP',[1,1,1;...
+                              6,1,1]...
+            };
     
-end
+        %}
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Read h5 data and save to timeseries collection
+    output_tscollect = Read_h5_save_tscollection(folderpath);
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Get names of timeseries in tscollection
+    tsnames = Get_output_tsnames(output_tscollect);
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Plot elements
+    if plot_elemt_flag
+
+        % plot requested data defined in plotElm_info
+        plot_elements(...
+            tsnames,...
+            output_tscollect,...
+            plotElm_info)
+
+    end
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Read timeseries names
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function stsnames = Get_output_tsnames(output_tscollect)
-    
-    % get number of timeseries
-    num_ts = numel(output_tscollect);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Read timeseries names
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function stsnames = Get_output_tsnames(output_tscollect)
 
-    % initiate stsnames cell
-    stsnames = cell(num_ts,1);
+        % get number of timeseries
+        num_ts = numel(output_tscollect);
 
-    for i = 1:num_ts
-        
-        % Get ts i
-        ts = output_tscollect{i};
-        
-        % Save name of ts i
-        stsnames{i} = ts.Name;
-        
+        % initiate stsnames cell
+        stsnames = cell(num_ts,1);
+
+        for i = 1:num_ts
+
+            % Get ts i
+            ts = output_tscollect{i};
+
+            % Save name of ts i
+            stsnames{i} = ts.Name;
+
+        end
+
     end
 
 end
-
 
 
 
