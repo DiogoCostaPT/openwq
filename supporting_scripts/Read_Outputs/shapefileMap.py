68aabb87afa7532bc9b5b39c69558a370ce55e7d
Diogo Costa <diogo.costa@usask.ca>
2023-02-15 03:25:56 +0000

    advancing mizuroute mapping python script

--- supporting_scripts/Read_Outputs/shapefileMap.py
+++ supporting_scripts/Read_Outputs/shapefileMap.py
@@ -1,34 +1,81 @@
+import netCDF4
+import h5py
 import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
 import contextily as cx
 
-# open it...
-# Shapefile
-shpfile_folder = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/mizuRoute/case_studies/testCase_cameo_v1.2/shapefile/shapefiles/'
-shpfile_file = 'Flowline_CO_14_cameo.shp'
-shpfile_fullpath = shpfile_folder + shpfile_file
+mainModelDir = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/mizuRoute/case_studies/testCase_cameo_v1.2/'
 
+# Shapefile River
+shpfile_subdir = 'shapefile/shapefiles/'
+shpfile_file = 'Flowline_CO_14_cameo.shp'
+shpfile_fullpath = mainModelDir + shpfile_subdir + shpfile_file
 geodf = gpd.read_file(shpfile_fullpath)
 geodf = geodf.to_crs("WGS84")
+comid = np.array(geodf['ComID'])
+
+# Adding WQ field in shapegfile
+geodf  ["openwq_wq"] = geodf["LengthKM"] * 0
+
+# Get NETOP info
+#netop_subdir = 'ancillary_data/'
+#netop_file = 'ntopo_nhdplus_cameo_pfaf.nc'
+#netcdf_file = netCDF4.Dataset(mainModelDir + netop_subdir + netop_file)
+#netopo = netcdf_file.variables['to'] # netopo key
+#netopo = np.array(netopo)
+
+# Openwq h5 results
+openwq_out_dir = 'Output_OpenWQ/HDF5/'
+openwq_out_file = 'RIVER_NETWORK_REACHES@SPECIES_A#G-main.h5'
+hf_file = h5py.File(mainModelDir + openwq_out_dir + openwq_out_file, 'r')
+
+# get list of h5 keys (timestamps)
+keys_list = list(hf_file.keys())
+
+# Get mapping of indexes
+#indexes = np.array()
+#for i in range(len(comid)):
+#    comid_i = comid[i]
+#    index_i = np.where()
+
+# mizuroute results
+mizuroute_out_dir = 'output/'
+mizuroute_out_fileame = 'v1.2_case1.h.1950-01-01-43200.nc' # just a sample for now
+mizuroute_out_file = netCDF4.Dataset(mainModelDir + mizuroute_out_dir + mizuroute_out_fileame)
+mizuroute_out_idFeature = mizuroute_out_file.variables['reachID']
+mizuroute_out_idFeature = np.array(mizuroute_out_idFeature)
 
 # Looping over time
-for i in range(10):
+for i in range(len(keys_list)):
 
-    # Addin WQ
-    geodf["openwq_wq"] = geodf["LengthKM"] * 0 + i
+    # Get data from timestamp i
+    h5_timestamp_vals = hf_file.get(keys_list[i])
+    h5_timestamp_vals = np.array(h5_timestamp_vals)
+
+    # loop over shapefile elements and get comid
+    # then find the correst output result
+    for j in range(len(comid)):
+        comid_j = comid[j]
+        index_j = np.where(mizuroute_out_idFeature == comid_j)
+        try:
+            geodf["openwq_wq"][j] = h5_timestamp_vals[0][index_j[0]]
+        except:
+            geodf["openwq_wq"][j] = -1
+
+
 
     # Plotting
-    ax = geodf.plot(column='openwq_wq', legend=True, vmin=0, vmax=10, figsize=(20, 14))
+    ax = geodf.plot(column='openwq_wq', legend=True, vmin=0, vmax=0.1, figsize=(20, 14))
     cx.add_basemap(ax, source=cx.providers.Stamen.TonerLite)
-    plt.draw()
     # plt.pause(0.05)
     plt.savefig('gif_results/' + str(i) + '.png')
+    plt.close()
 
 # Build GIF
 import imageio.v2
 
 with imageio.get_writer('gif_results/results_openwq.gif', mode='I') as writer:
-    for t in range(10):
+    for t in range(len(comid)):
         image = imageio.v2.imread('gif_results/' + str(t) + '.png')
         writer.append_data(image)
